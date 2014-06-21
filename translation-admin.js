dfb15c6ccf84953b658685e820e3064975d6c356
Moïse Valvassori <m11.valvassori@gmail.com>
2014-06-22 01:19:14 +0200

    Update and insert translation

--- translation-admin.js
+++ translation-admin.js
@@ -46,6 +46,11 @@ if (Meteor.isClient) {
         return Translation.collection.find({});
     };
 
+    Template.search_translation.newline = function(){
+        return Session.get('translation_edit') === 'new';
+    };
+
+
     Template.search_translation.events({
 	"change #filterDomain, keyup #filterDomain" : function (e,tpl) {
 	    var val = $(e.target).val();
@@ -62,19 +67,76 @@ if (Meteor.isClient) {
 	"change #filterValue, keyup #filterValue" : function (e,tpl) {
 	    var val = $(e.target).val();
 	    Session.set("translationSearch_value", val);
+	},
+	"click #add_line" : function() {
+	    Session.set('translation_edit', "new");
 	}
     });
 
     Template.table_item.events({
         'click .view' : function (e, tpl){
-            console.log(tpl.data);
             Session.set('translation_edit', tpl.data._id);
-        }
+        },
+	'click #validate': function(e, tpl){
+	    var set={};
+	    var value = $(tpl.find('.ivalue')).val().trim();
+	    if (!_.isEmpty(value) && tpl.data.value !== value)
+		set.value = value;
+	    var key = $(tpl.find('.ikey')).val().trim();
+	    if (!_.isEmpty(key) && tpl.data.key !== key)
+		set.key = key;
+	    var lang = $(tpl.find('.ilang')).val().trim().split(',');
+	    if (!_.isEmpty($(tpl.find('.ilang')).val().trim()) && tpl.data.lang != lang)
+		set.lang = lang;
+	    var domain = $(tpl.find('.idomain')).val().trim().split(',');
+	    if (!_.isEmpty($(tpl.find('.idomain')).val().trim()) && tpl.data.domain != domain)
+		set.domain = domain;
+
+	    console.log(set);
+	    if (! _.isEmpty(set)){
+		Translation.collection.update(tpl.data._id, {$set: set});
+		Session.set('translation_edit', undefined);
+	    }
+	}
     });
 
     Template.table_item.edit = function(){
         return this._id === Session.get('translation_edit');
-    }
+    };
+
+    Template.new_table_item.events({
+	'click #validate': function(e, tpl){
+	    var set={};
+	    var value = $(tpl.find('.ivalue')).val().trim();
+	    if (!_.isEmpty(value))
+		set.value = value;
+	    else
+		return;
+	    var key = $(tpl.find('.ikey')).val().trim();
+	    if (!_.isEmpty(key))
+		set.key = key;
+	    else
+		return;
+	    var lang = $(tpl.find('.ilang')).val().trim().split(',');
+	    if (!_.isEmpty($(tpl.find('.ilang')).val().trim()))
+		set.lang = lang;
+	    else
+		return;
+	    var domain = $(tpl.find('.idomain')).val().trim().split(',');
+	    if (!_.isEmpty($(tpl.find('.idomain')).val().trim()))
+		set.domain = domain;
+	    else
+		return;
+
+	    console.log(set);
+	    if (! _.isEmpty(set)){
+		Translation.collection.insert(set);
+		Session.set('translation_edit', undefined);
+	    }
+	}
+    });
+
+
 }
 
 if (Meteor.isServer) {
