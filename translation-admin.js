94a3b3827dc5d59d969d3c81aeb51857d5a05c7d
Moïse Valvassori <m11.valvassori@gmail.com>
2014-06-21 20:02:15 +0200

    filter the translation

--- translation-admin.js
+++ translation-admin.js
@@ -23,6 +23,14 @@ Translation.collection.attachSchema(Schemas.Translations);
 
 
 if (Meteor.isClient) {
+    Deps.autorun(function () {
+	Meteor.subscribe("translationSearch",
+			 Session.get("translationSearch_domain"),
+			 Session.get("translationSearch_key"),
+			 Session.get("translationSearch_lang"),
+			 Session.get("translationSearch_value"));
+    });
+
     Template.insert_translation.collection = function(){
         return Translation.collection;
     };
@@ -30,10 +38,43 @@ if (Meteor.isClient) {
     Template.search_translation.search_result = function(){
         return Translation.collection.find({});
     };
+
+    Template.search_translation.events({
+	"change #filterDomain, keyup #filterDomain" : function (e,tpl) {
+	    var val = $(e.target).val();
+	    Session.set("translationSearch_domain", val);
+	},
+	"change #filterKey, keyup #filterKey" : function (e,tpl) {
+	    var val = $(e.target).val();
+	    Session.set("translationSearch_key", val);
+	},
+	"change #filterLang, keyup #filterLang" : function (e,tpl) {
+	    var val = $(e.target).val();
+	    console.log(val);
+	    Session.set("translationSearch_lang", val);
+	},
+	"change #filterValue, keyup #filterValue" : function (e,tpl) {
+	    var val = $(e.target).val();
+	    Session.set("translationSearch_value", val);
+	}
+    });
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        // code to run on server at startup
+	Meteor.publish('translationSearch', 
+		       function(domain, key, lang, value){
+			   return Translation.collection.find({domain: {$regex: domain},
+							       key: {$regex: key},
+							       lang: {$regex: lang},
+							       value: {$regex: value}
+							      },
+							      {
+								  sort: ["domain", "key"],
+								  limit: 20,
+								  skip: 0
+							      }
+							     );
+		       });
     });
 }
